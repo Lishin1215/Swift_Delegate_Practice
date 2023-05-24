b61fdebcc7eba6cf81bca26be48bd10a7202702c
Lishin <99582713+Lishin1215@users.noreply.github.com>
2023-05-24 23:43:57 +0800

    add dataSource

--- DI Part 1 - Delegate/SelectionView.swift
+++ DI Part 1 - Delegate/SelectionView.swift
@@ -16,21 +16,34 @@ class SelectionView: UIView {
     private var customView: UIView!
     
     //初始化方法
-    convenience init(buttonCount: Int) {
-        self.init(frame: .zero)
-        createButtons(count:buttonCount)
-    }
+//    convenience init(buttonCount: Int) {
+//        self.init(frame: .zero)
+//        createButtons(count:buttonCount)
+//    }
+//
+//    //初始化方法
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        setUpView()
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//            super.init(coder: aDecoder)
+//            setUpView()
+//        }
     
-    //初始化方法
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    init(){
+        super.init(frame: .zero)
         setUpView()
+        // 根據這個sectionView(self)是Top/ Bottom去放入對應的按鈕數量
+        createButtons(count: dataSource?.numberOfOptions(for: self) ?? 0)
+        
     }
     
-    required init?(coder aDecoder: NSCoder) {
-            super.init(coder: aDecoder)
-            setUpView()
-        }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     
     private func setUpView() {
         backgroundColor = .black
@@ -43,9 +56,27 @@ class SelectionView: UIView {
         // Create custom view
         customView = UIView()
         addSubview(customView)
-        
+       
         // Add constraints
+       translatesAutoresizingMaskIntoConstraints = false
+       indicatorView.translatesAutoresizingMaskIntoConstraints = false
+       customView.translatesAutoresizingMaskIntoConstraints = false
         
+        // Set constraints for indicator view
+        NSLayoutConstraint.activate([
+            indicatorView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 35),
+            indicatorView.topAnchor.constraint(equalTo: buttons[0].bottomAnchor),
+            indicatorView.widthAnchor.constraint(equalToConstant: 70),
+            indicatorView.heightAnchor.constraint(equalToConstant: 2)
+        ])
+                
+        // Set constraints for custom view
+        NSLayoutConstraint.activate([
+            customView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+            customView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 15),
+            customView.topAnchor.constraint(equalTo: indicatorView.bottomAnchor),
+            customView.heightAnchor.constraint(equalToConstant: 50)
+        ])
         
     }
     
@@ -56,14 +87,22 @@ class SelectionView: UIView {
             button.backgroundColor = .clear
             buttons.append(button)
             addSubview(button)
+            
+            // Add constraints for buttons
+            button.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                button.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 35),
+                button.topAnchor.constraint(equalTo: topAnchor, constant: 40),
+                button.widthAnchor.constraint(equalTo: widthAnchor),
+                button.heightAnchor.constraint(equalToConstant: 40)
+            ])
+            
         }
-        
-        // Add constraints for buttons
-        
         
     }
     
     weak var dataSource: SelectionViewDataSource?
+    weak var delegate: SelectionViewDelegate?
     
     
     
--- DI Part 1 - Delegate/ViewController.swift
+++ DI Part 1 - Delegate/ViewController.swift
@@ -9,12 +9,13 @@ import UIKit
 
 protocol SelectionViewDataSource: AnyObject {
     
-    var numberOfOptions: Int {get}
+    func numberOfOptions(for selectionView: SelectionView) -> Int
     func textForOption(at index: Int) -> String
-    var underlineColor: UIColor {get}
+    var underlineColor: UIColor {get} //只能取得數字，不能設定
     var optionTextColor: UIColor {get}
     var optionTextFont: UIFont {get}
 
+    
 }
 
 @objc protocol SelectionViewDelegate: AnyObject {
@@ -27,7 +28,32 @@ protocol SelectionViewDataSource: AnyObject {
 
 class ViewController: UIViewController, SelectionViewDataSource, SelectionViewDelegate {
     
-    var numberOfOptions: Int = 2
+
+
+    let selectionViewTop = SelectionView()
+    let selectionViewBottom = SelectionView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        selectionViewTop.dataSource = self
+        selectionViewTop.delegate = self
+        
+        selectionViewBottom.dataSource = self
+        selectionViewBottom.delegate = self
+        
+    }
+    
+//MARK: DataSource
+    
+    func numberOfOptions(for selectionView: SelectionView) -> Int{
+        if selectionView == selectionViewTop{
+            return 2
+        } else if selectionView == selectionViewBottom{
+            return 3
+        }
+        return 0
+    }
     
     func textForOption(at index: Int) -> String {
         if index == 0 {
@@ -45,18 +71,6 @@ class ViewController: UIViewController, SelectionViewDataSource, SelectionViewDe
     
     var optionTextFont: UIFont = UIFont.systemFont(ofSize: 18)
     
-
-    let selectionView = SelectionView()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        selectionView.dataSource = self
-        selectionView.delegate = self
-        
-    }
-    
-
     
 
 }
