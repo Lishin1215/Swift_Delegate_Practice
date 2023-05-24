217d98b138886a06b382737e9777d1af77a83ae4
Lishin <99582713+Lishin1215@users.noreply.github.com>
2023-05-24 17:04:07 +0800

    build a new project and add .gitignore document

---
+++ DI Part 1 - Delegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DI Part 1 - Delegate
+//
+//  Created by 簡莉芯 on 2023/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
